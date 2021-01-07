92737711cebc03938737147d69f34590e489e0c9
Adrian Voicu <adrianvoicu.tm@gmail.com>
2021-01-07 15:18:33 +0100

    P4

--- network.py
+++ network.py
@@ -1,26 +1,40 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
 
-weights = [ [0.2, 0.8, -0.5, 1.0],
-			[0.5, -0.91, 0.26, -0.5],
-			[-0.26, -0.27, 0.17, 0.87]]
+# Input data
+# 	number of samples = 3
+# 	number of features per sample = 4
 
-biases = [2, 3, 0.5]
-
-# output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
-# 		  inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
-# 		  inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3]
-
-# layer_outputs = []
-# for neuron_weights, bias in zip(weights,biases):
-# 	neuron_output = 0
-# 	for n_input, weight in zip(inputs,neuron_weights):
-# 		neuron_output += n_input*weight
-# 	layer_outputs.append(neuron_output+bias)
-
-output = np.dot(weights, inputs) + biases
+X = [[1, 2, 3, 2.5],
+	  [2.0, 5.0, -1.0, 2.0],
+	  [-1.5, 2.7, 3.3, -0.8]]
 
 
-print(layer_outputs)
-print(output)
+np.random.seed(0)
+
+
+class Layer_Dense:
+	'''
+		n_inputs = number of features (per data sample or per output of the previous layer)
+		n_neurons = number of neurons (per layer) 
+	'''
+	def __init__(self, n_inputs, n_neurons):
+
+		# Returns an array of shape n_inputs x n_neurons of the standard normal distribution
+		self.weights = 0.10 * np.random.randn(n_inputs, n_neurons) 
+		
+		# Returns a zero array of shape 1 x n_neurons 
+		self.biases = np.zeros((1, n_neurons))
+		
+
+	def forward(self, inputs):
+
+		self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
